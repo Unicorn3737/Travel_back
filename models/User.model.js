bf9b27aaeb44ae2ba74e66ed7f116ed8f4952cf4
Anna Turek <anutkakpi@ukr.net>
2025-02-18 22:23:49 +0200

    Deploy

--- models/User.model.js
+++ models/User.model.js
@@ -23,6 +23,10 @@ const userSchema = new Schema(
       default:
         "https://images.unsplash.com/photo-1516708274537-6f91e34ccaf2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
+    trips: {
+      type: [Schema.Types.ObjectId],
+      ref: "Drive",
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
